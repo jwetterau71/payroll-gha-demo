0cc9fe36c50d607da8bb2b10478c9390f048edc2
Jeff Wetterau <jwetterau@gmail.com>
2023-04-14 10:19:22 -0500

    initial commit

---
+++ src/main/java/com/wettj/payroll/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package com.wettj.payroll;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
---
+++ src/test/java/com/wettj/payroll/PayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wettj.payroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
